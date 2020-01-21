fae553a643e9c36e9c5493282abec50cb2aa875e
Marco Elver <elver@google.com>
2020-01-21 17:05:12 +0100

    copy_to_user, copy_from_user: Use generic instrumented.h
    This replaces the KASAN instrumentation with generic instrumentation,
    implicitly adding KCSAN instrumentation support.
    
    For KASAN no functional change is intended.
    
    Suggested-by: Arnd Bergmann <arnd@arndb.de>
    Signed-off-by: Marco Elver <elver@google.com>
    Signed-off-by: Paul E. McKenney <paulmck@kernel.org>
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- lib/usercopy.c
+++ lib/usercopy.c
@@ -1,7 +1,8 @@
 // SPDX-License-Identifier: GPL-2.0
-#include <linux/uaccess.h>
 #include <linux/bitops.h>
 #include <linux/nospec.h>
+#include <linux/instrumented.h>
+#include <linux/uaccess.h>
 
 /* out-of-line parts */
 
@@ -17,7 +18,7 @@ unsigned long _copy_from_user(void *to, const void __user *from, unsigned long n
 		 * finished:
 		 */
 		barrier_nospec();
-		kasan_check_write(to, n);
+		instrument_copy_from_user(to, from, n);
 		res = raw_copy_from_user(to, from, n);
 	}
 	if (unlikely(res))
@@ -32,7 +33,7 @@ unsigned long _copy_to_user(void __user *to, const void *from, unsigned long n)
 {
 	might_fault();
 	if (likely(access_ok(to, n))) {
-		kasan_check_read(from, n);
+		instrument_copy_to_user(to, from, n);
 		n = raw_copy_to_user(to, from, n);
 	}
 	return n;
