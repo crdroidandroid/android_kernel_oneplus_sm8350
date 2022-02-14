3a84037b3777896d78b3a97793f0a5204126fed9
Pankaj Gupta <quic_gpankaj@quicinc.com>
2022-02-14 10:38:12 +0530

    Revert "msm: kgsl: Call dma_buf_unmap_attachment() early"
    This reverts commit 0e47811b36fd7d0a045a7f188dc94194758992e8.
    
    This patch is reported to be causing screen corruption issues
    with video use cases, so reverting this change.
    
    Signed-off-by: Pankaj Gupta <quic_gpankaj@quicinc.com>
    Change-Id: Ie125949be4b50888f0da302dba037d66d71f40b6
    Signed-off-by: Divyanshu-Modi <divyan.m05@gmail.com>
    Signed-off-by: TogoFire <togofire@mailfence.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- drivers/gpu/msm/kgsl.c
+++ drivers/gpu/msm/kgsl.c
@@ -344,6 +344,8 @@ static void kgsl_destroy_ion(struct kgsl_memdesc *memdesc)
 
 	if (meta != NULL) {
 		remove_dmabuf_list(meta);
+		dma_buf_unmap_attachment(meta->attach, meta->table,
+			DMA_BIDIRECTIONAL);
 		dma_buf_detach(meta->dmabuf, meta->attach);
 		dma_buf_put(meta->dmabuf);
 		kfree(meta);
@@ -2990,7 +2992,7 @@ static int kgsl_setup_dma_buf(struct kgsl_device *device,
 {
 	int ret = 0;
 	struct scatterlist *s;
-	struct sg_table *sg_table;
+	struct sg_table *sg_table = NULL;
 	struct dma_buf_attachment *attach = NULL;
 	struct kgsl_dma_buf_meta *meta;
 
@@ -3031,8 +3033,6 @@ static int kgsl_setup_dma_buf(struct kgsl_device *device,
 		ret = PTR_ERR(sg_table);
 		goto out;
 	}
-
-	dma_buf_unmap_attachment(attach, sg_table, DMA_BIDIRECTIONAL);
 
 	meta->table = sg_table;
 	entry->priv_data = meta;
@@ -3088,6 +3088,9 @@ skip_access_check:
 
 out:
 	if (ret) {
+		if (!IS_ERR_OR_NULL(sg_table))
+			dma_buf_unmap_attachment(attach, sg_table, DMA_BIDIRECTIONAL);
+
 		if (!IS_ERR_OR_NULL(attach))
 			dma_buf_detach(dmabuf, attach);
 
