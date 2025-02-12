f701338cc173af8d81afe11c140cf9128cffcfae
Pranav Vashi <neobuddy89@gmail.com>
2025-02-12 20:30:45 +0200

    drivers: Import KernelSU rsuntk 12051 with SUSFS v1.5.5 support

---
+++ drivers/kernelsu/manager_sign.h
@@ -0,0 +1,12 @@
+#ifndef MANAGER_SIGN_H
+#define MANAGER_SIGN_H
+
+// rsuntk/KernelSU
+#define EXPECTED_SIZE_RSUNTK    0x396
+#define EXPECTED_HASH_RSUNTK    "f415f4ed9435427e1fdf7f1fccd4dbc07b3d6b8751e4dbcec6f19671f427870b"
+
+// 5ec1cff/KernelSU
+#define EXPECTED_SIZE_5EC1CFF   0x384
+#define EXPECTED_HASH_5EC1CFF   "7e0c6d7278a3bb8e364e0fcba95afaf3666cf5ff3c245a3b63c8833bd0445cc4"
+
+#endif /* MANAGER_SIGN_H */
