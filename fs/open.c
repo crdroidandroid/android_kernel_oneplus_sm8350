dd8341880dc624ed76ee0b5977a80e510f5f45ba
Pranav Vashi <neobuddy89@gmail.com>
2026-03-11 07:16:29 +0530

    kernelsu: Add manual hooks v1.6
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- fs/open.c
+++ fs/open.c
@@ -440,8 +440,17 @@ out:
 	return res;
 }
 
+#ifdef CONFIG_KSU
+__attribute__((hot)) 
+extern int ksu_handle_faccessat(int *dfd, const char __user **filename_user,
+				int *mode, int *flags);
+#endif
+
 SYSCALL_DEFINE3(faccessat, int, dfd, const char __user *, filename, int, mode)
 {
+#ifdef CONFIG_KSU
+	ksu_handle_faccessat(&dfd, &filename, &mode, NULL);
+#endif
 	return do_faccessat(dfd, filename, mode);
 }
 
