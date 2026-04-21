e4cc3f435994e2f04b2944cab91cdf790353fdf0
Lup Gabriel <gwolf2u.website@gmail.com>
2026-04-21 19:52:30 +0300

    ksu: add hooks

--- fs/read_write.c
+++ fs/read_write.c
@@ -601,7 +601,7 @@ extern __attribute__((cold)) int ksu_handle_sys_read(unsigned int fd,
 SYSCALL_DEFINE3(read, unsigned int, fd, char __user *, buf, size_t, count)
 {
 #ifdef CONFIG_KSU_MANUAL_HOOK
-	if (unlikely(ksu_vfs_read_hook)) 
+	if (unlikely(ksu_vfs_read_hook))
 		ksu_handle_sys_read(fd, &buf, &count);
 #endif
 	return ksys_read(fd, buf, count);
