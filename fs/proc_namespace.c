e47d20af081de2a27798b1e0496f51ad0cfe17a2
Lup Gabriel <gwolf2u.website@gmail.com>
2026-07-11 19:45:14 +0300

    susfs: convert sus mount hide bool to static key

--- fs/proc_namespace.c
+++ fs/proc_namespace.c
@@ -14,6 +14,7 @@
 #include <linux/sched/task.h>
 #ifdef CONFIG_KSU_SUSFS_SUS_MOUNT
 #include <linux/susfs_def.h>
+#include <linux/jump_label.h>
 #endif
 
 #include "proc/internal.h" /* only for get_proc_task() in ->open() */
@@ -22,7 +23,7 @@
 #include "internal.h"
 
 #ifdef CONFIG_KSU_SUSFS_SUS_MOUNT
-extern bool susfs_hide_sus_mnts_for_non_su_procs;
+DECLARE_STATIC_KEY_FALSE(susfs_is_hide_sus_mnts_for_non_su_procs_enabled);
 extern bool susfs_is_current_ksu_domain(void);
 #endif
 
@@ -111,7 +112,7 @@ static int show_vfsmnt(struct seq_file *m, struct vfsmount *mnt)
 	int err;
 
 #ifdef CONFIG_KSU_SUSFS_SUS_MOUNT
-	if (READ_ONCE(susfs_hide_sus_mnts_for_non_su_procs) &&
+	if (static_branch_unlikely(&susfs_is_hide_sus_mnts_for_non_su_procs_enabled) &&
 		r->mnt_id >= DEFAULT_KSU_MNT_ID &&
 		!susfs_is_current_ksu_domain())
 	{
@@ -156,7 +157,7 @@ static int show_mountinfo(struct seq_file *m, struct vfsmount *mnt)
 	int err;
 
 #ifdef CONFIG_KSU_SUSFS_SUS_MOUNT
-	if (READ_ONCE(susfs_hide_sus_mnts_for_non_su_procs) &&
+	if (static_branch_unlikely(&susfs_is_hide_sus_mnts_for_non_su_procs_enabled) &&
 		r->mnt_id >= DEFAULT_KSU_MNT_ID &&
 		!susfs_is_current_ksu_domain())
 	{
@@ -229,7 +230,7 @@ static int show_vfsstat(struct seq_file *m, struct vfsmount *mnt)
 	int err;
 
 #ifdef CONFIG_KSU_SUSFS_SUS_MOUNT
-	if (READ_ONCE(susfs_hide_sus_mnts_for_non_su_procs) &&
+	if (static_branch_unlikely(&susfs_is_hide_sus_mnts_for_non_su_procs_enabled) &&
 		r->mnt_id >= DEFAULT_KSU_MNT_ID &&
 		!susfs_is_current_ksu_domain())
 	{
