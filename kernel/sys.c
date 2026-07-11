974a7b7a3b3c72429e06146649b1054894f7e415
Lup Gabriel <gwolf2u.website@gmail.com>
2026-07-11 19:51:22 +0300

    susfs: update all kernel hook sites for renamed static keys

--- kernel/sys.c
+++ kernel/sys.c
@@ -1251,7 +1251,7 @@ static int override_release(char __user *release, size_t len)
 }
 
 #ifdef CONFIG_KSU_SUSFS_SPOOF_UNAME
-extern struct static_key_true susfs_set_uname_key_true;
+DECLARE_STATIC_KEY_FALSE(susfs_is_uname_spoof_buffer_set);
 extern void susfs_spoof_uname(struct new_utsname* tmp);
 #endif
 SYSCALL_DEFINE1(newuname, struct new_utsname __user *, name)
@@ -1261,7 +1261,7 @@ SYSCALL_DEFINE1(newuname, struct new_utsname __user *, name)
 	down_read(&uts_sem);
 	memcpy(&tmp, utsname(), sizeof(tmp));
 #ifdef CONFIG_KSU_SUSFS_SPOOF_UNAME
-	if (static_branch_likely(&susfs_set_uname_key_true))
+	if (static_branch_unlikely(&susfs_is_uname_spoof_buffer_set))
 		susfs_spoof_uname(&tmp);
 #endif
 	up_read(&uts_sem);
